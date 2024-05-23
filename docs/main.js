568d5c88d5443a8194700d0221b72d066cf1a03b
Jack <wzhang3912@gmail.com>
2024-05-23 16:01:39 -0700

    website setup

---
+++ docs/main.js
@@ -0,0 +1,80 @@
+let imagePaths;
+
+// function to load images data
+async function load() {
+    // TODO: upload images to cdn providers(AWS Cloudfront or Cloudflare) and use cdn link instead
+    const data = await d3.json('/data/small_img_data.json');
+    return data
+}
+
+// preload images
+// async function preloadImages(paths) {
+//     //let loadedImages = 0;
+//     let images = [];
+    
+//     paths.forEach((path, index) => {
+//         // preloading
+//         const img = new Image();
+//         img.src = '/data/' + path.path;
+        
+//         images[index] = img;
+        
+//         // images[index].onload = () => {
+//         //     loadedImages++;
+//         //     if (loadedImages === paths.length) {
+//         //         callback(images);  // All images are loaded, call the callback
+//         // };
+//         //     }
+
+//         images[index].onerror = () => {
+//             console.error(`Error loading image: ${path.path}`);
+//         };
+//     });
+
+//     return images
+// }
+
+
+function drawSVG() {
+
+    const margin = {top: 20, right: 20, bottom: 30, left: 32},
+    width = 1400 - margin.left - margin.right, 
+    height = 800 - margin.top - margin.bottom; 
+
+    // Append the svg object to the body of the page
+    svg = d3.select("#imageVis")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    svg.selectAll("image")
+    .data(imagePaths)
+    .enter()
+    .append("svg:image")
+    .attr('xlink:href', (d) => (d.path))
+    .attr('x', d3.randomInt(0, width))
+    .attr('y', d3.randomInt(0, height))
+    .attr('width', 100)
+    .attr('height', 100);
+    
+}
+
+// async load and parse data 
+load().then(imgPaths => {
+
+    imagePaths = imgPaths;
+
+    drawSVG();
+
+    // preloadImages(imagePaths).then(img => {
+
+    //     // imagesData = img;
+
+    //     drawSVG();
+
+    // });
+
+})
+
---
+++ docs/scroll.js
@@ -0,0 +1,103 @@
+let container = d3.select("#scroll").select(".scroll__container");
+
+let text = container.select(".scroll__text");
+let imageVis = container.select(".scroll__vis").select('#imageVis');
+let steps = text.selectAll(".step");
+
+
+// initialize the scrollama
+var scroller = scrollama();
+
+// Initialize the visualization (example using D3.js)
+const svg = d3.select("#imageVis")
+    .append("svg")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .append("g");
+
+// Example visualization elements
+svg.append("circle")
+    .attr("cx", "50%")
+    .attr("cy", "50%")
+    .attr("r", 50)
+    .attr("fill", "blue");
+
+// generic window resize listener event
+function handleResize() {
+	// 1. update height of step elements
+	var stepH = Math.floor(window.innerHeight * 0.75);
+	steps.style('top', 100 + 'px')
+	.style("height", stepH + "px");
+
+	// 2. update height of graphic element
+	var bodyWidth = d3.select('body').node().offsetWidth;
+
+	// graphic.style('height', window.innerHeight + 'px');
+
+	// 3. update width of chart by subtracting from text width
+	// make the height 1/2 of viewport
+	var chartHeight = Math.floor(window.innerHeight / 2);
+
+	imageVis.style('height', chartHeight + 'px');
+
+	// 3. tell scrollama to update new element dimensions
+	scroller.resize();
+}
+
+// scrollama event handlers
+function handleStepEnter(response) {
+	// response = { element, direction, index }
+
+	// add color to current step only
+	steps.classed("is-active", function (d, i) {
+		return i === response.index;
+	});
+
+	// update graphic based on step
+
+	const index = response.index;
+	console.log(index);
+	
+	switch (index) {
+        case 0:
+            svg.select("circle")
+                .transition()
+                .attr("fill", "blue")
+                .attr("r", 50);
+            break;
+        case 1:
+            svg.select("circle")
+                .transition()
+                .attr("fill", "green")
+                .attr("r", 100);
+            break;
+        case 2:
+            svg.select("circle")
+                .transition()
+                .attr("fill", "red")
+                .attr("r", 150);
+            break;
+    }
+}
+
+function init() {
+
+	// 1. force a resize on load to ensure proper dimensions are sent to scrollama
+	handleResize();
+
+	// 2. bind scrollama event handlers (this can be chained like below)
+	scroller
+		.setup({
+			container: ".scroll__container",
+			step: ".step",
+			offset: 0.5,
+			debug: true
+		})
+		.onStepEnter(handleStepEnter);
+	
+	window.addEventListener("resize", handleResize);
+
+}
+
+// initialize
+init();
