e5499e6557a041aeab42c1ecb9172c1608253cfb
Jack <wzhang3912@gmail.com>
2024-05-29 15:13:16 -0700

    update path

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,5 @@
-const img_path_url = 'asset/cloud_image_data.json'
+// const img_path_url = 'asset/cloud_image_data.json'
+const img_path_url = 'https://res.cloudinary.com/duyoevfl6/raw/upload/v1717020699/DSC106%20MET%20Images/cloud_path.json'
 const knn_ex_url = 'asset/knn_ex_network.json'
 
 let imagePaths;
