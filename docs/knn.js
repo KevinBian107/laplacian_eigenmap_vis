75d16fe743f037d8e9ee44c9cb742c233eb8b3dc
Kevin Bian <kaiwenbian107@gmail.com>
2024-06-03 22:41:30 -0700

    add

--- docs/knn.js
+++ docs/knn.js
@@ -1,6 +1,6 @@
 import { load, imagePathsData } from './main.js';
 
-const knn_ex_url = '/asset/knn_ex_network_new.json'
+const knn_ex_url = 'https://raw.githubusercontent.com/KevinBian107/laplacian_eigenmap_vis/master/asset/knn_ex_network_new.json'
 // const knn_ex_url = 'https://res.cloudinary.com/duyoevfl6/raw/upload/v1717021257/DSC106%20MET%20Images/knn_path.json'
 
 const margin = {top: 0, right: 70, bottom: 0, left: 70}, 
