bc7f88ef330a2ee3333f3a891dfb45cad0da4e55
Jack <wzhang3912@gmail.com>
2024-06-04 11:49:44 -0700

    update coord

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,4 @@
-const img_path_url = 'https://raw.githubusercontent.com/KevinBian107/laplacian_eigenmap_vis/master/asset/local_image_data_new.json'
+const img_path_url = 'https://raw.githubusercontent.com/KevinBian107/laplacian_eigenmap_vis/master/asset/image_data.json'
 
 // const img_path_url = 'https://res.cloudinary.com/duyoevfl6/raw/upload/v1717020699/DSC106%20MET%20Images/cloud_path.json'
 
